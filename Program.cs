81505754d0a61385f39ca0d7808091c46eb0633e
Roman Filatov <ikardoc@gmail.com>
2022-04-24 01:21:38 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+class Product
+{
+    public int Id;
+
+    public string Description;
+
+    public double Price;
+
+    public string Currency;
+}
+
+
+abstract class Delivery
+{
+    public string Address;
+}
+
+class HomeDelivery : Delivery
+{
+    public double DeliveryPrice;
+}
+
+class PickPointDelivery : Delivery
+{
+    public double DeliveryPrice;
+}
+
+class ShopDelivery : Delivery
+{
+    public double DeliveryPrice;
+}
+
+class Order<TDelivery, TStruct> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+
+    public int Number;
+
+    public string Description;
+
+    public List<Product> Products; 
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+}
